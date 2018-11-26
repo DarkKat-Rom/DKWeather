7699c6bc97c0d5956c69be4476b0e5da0b87490e
XXMrHyde <xxmrhyde@gmx.co.uk>
2018-11-26 13:36:03 +0100

    Use RecyclerView for weather cards, (wip, part 1):
    (Add new layouts and classes)
    
    This does nothing more than adding a new Activity holding a fragment
    which displays the current temperature and weather data time.
    There is still an fully functional update button on the action bar.
    The new activity will only be shown, when stating the app via launcher,
    actions on the widget, notification and launcher shortcuts will still
    start the 'old' activities.

---
+++ app/src/main/java/net/darkkatrom/dkweather/activities/MainActivityNew.java
@@ -0,0 +1,217 @@
+/*
+ * Copyright (C) 2018 DarkKat
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.darkkatrom.dkweather.activities;
+
+import android.app.Fragment;
+import android.content.ContentResolver;
+import android.database.ContentObserver;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.view.View.OnClickListener;
+import android.view.View.OnLongClickListener;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import net.darkkatrom.dkweather.R;
+import net.darkkatrom.dkweather.WeatherInfo;
+import net.darkkatrom.dkweather.fragments.WeatherFragmentNew;
+import net.darkkatrom.dkweather.utils.Config;
+import net.darkkatrom.dkweather.utils.JobUtil;
+import net.darkkatrom.dkweather.utils.NotificationUtil;
+
+public class MainActivityNew extends BaseActivity implements OnClickListener, OnLongClickListener {
+    private static final String TAG = "DKWeather:MainActivity";
+
+    private static final Uri WEATHER_URI =
+            Uri.parse("content://net.darkkatrom.dkweather.provider/weather");
+
+    private static final int TOAST_SPACE_TOP = 24;
+
+    private Handler mHandler;
+    private ContentResolver mResolver;
+    private WeatherObserver mWeatherObserver;
+
+    private WeatherInfo mWeatherInfo;
+    private long mTimestamp = -1;
+
+    private boolean mUpdateRequested = false;
+
+    private ImageView mUpdateButton;
+
+    private WeatherFragmentNew mFragment = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mHandler = new Handler();
+        mResolver = getContentResolver();
+        mWeatherObserver = new WeatherObserver(mHandler);
+
+        NotificationUtil notificationUtil = new NotificationUtil(this);
+        notificationUtil.setNotificationChannels();
+        mFragment = new WeatherFragmentNew();
+        getFragmentManager().beginTransaction()
+                .replace(R.id.fragment_content, mFragment)
+                .commit();
+    }
+
+    @Override
+    protected int getLayoutResId() {
+        return R.layout.main_new;
+    }
+
+    @Override
+    protected boolean checkForThemeUpdate() {
+        return true;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        mWeatherObserver.observe();
+        long newTimestamp = 0;
+        WeatherInfo newWeatherInfo = Config.getWeatherData(this);
+        if (newWeatherInfo != null) {
+            newTimestamp = newWeatherInfo.getTimestamp();
+        }
+        if (mTimestamp != newTimestamp) {
+            updateWeather();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mWeatherObserver.unobserve();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_update, menu);
+        MenuItem itemUpdate = menu.findItem(R.id.item_update);
+        LinearLayout updateButtonLayout = (LinearLayout) itemUpdate.getActionView();
+        mUpdateButton = (ImageView) updateButtonLayout.findViewById(R.id.update_button);
+
+        updateButtonLayout.setOnClickListener(this);
+        updateButtonLayout.setOnLongClickListener(this);
+        return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.update_button_layout) {
+            RotateAnimation anim = new RotateAnimation(0.0f, 360.0f,
+                    Animation.RELATIVE_TO_SELF, 0.5f,
+                    Animation.RELATIVE_TO_SELF, 0.5f);
+            anim.setInterpolator(new LinearInterpolator());
+            anim.setDuration(700);
+            anim.setAnimationListener(new AnimationListener() {
+
+                @Override
+                public void onAnimationStart(Animation animation) {
+                }
+
+                @Override
+                public void onAnimationEnd(Animation animation) {
+                    mUpdateButton.setAnimation(null);
+                }
+
+                @Override
+                public void onAnimationRepeat(Animation animation) {
+                }
+            });
+            mUpdateButton.startAnimation(anim);
+            mUpdateRequested = true;
+            JobUtil.startUpdate(this);
+        }
+    }
+
+    @Override
+    public boolean onLongClick(View v) {
+        if (v.getId() == R.id.update_button_layout) {
+            showToast(R.string.menu_item_update_weather_title);
+            return true;
+        }
+        return false;
+    }
+
+    private void showToast(int resId) {
+		float density = getResources().getDisplayMetrics().density;
+        int actionBarHeight = getSupportActionBar().getHeight();
+        int spaceTopDP = TOAST_SPACE_TOP * Math.round(density);
+
+        Toast toast = Toast.makeText(this, resId, Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.TOP | Gravity.CENTER_HORIZONTAL, 0, actionBarHeight + spaceTopDP);
+        toast.show();
+    }
+
+    public void updateWeather() {
+        mWeatherInfo = Config.getWeatherData(this);
+        if (mWeatherInfo == null) {
+            mTimestamp = 0;
+        } else {
+            mTimestamp = mWeatherInfo.getTimestamp();
+        }
+        updateContent();
+    }
+
+    private void updateContent() {
+        mFragment.updateContent(mWeatherInfo);
+    }
+
+    class WeatherObserver extends ContentObserver {
+        WeatherObserver(Handler handler) {
+            super(handler);
+        }
+
+        void observe() {
+            mResolver.registerContentObserver(WEATHER_URI, false, this);
+        }
+
+        void unobserve() {
+            mResolver.unregisterContentObserver(this);
+        }
+
+        @Override
+        public void onChange(boolean selfChange) {
+            updateWeather();
+            if (mWeatherInfo == null) {
+                Log.e(TAG, "Error retrieving weather data");
+                if (mUpdateRequested) {
+                    mUpdateRequested = false;
+                }
+            } else {
+                if (mUpdateRequested) {
+                    showToast(R.string.weather_updated);
+                    mUpdateRequested = false;
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/net/darkkatrom/dkweather/fragments/WeatherFragmentNew.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2018 DarkKat
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.darkkatrom.dkweather.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import net.darkkatrom.dkweather.R;
+import net.darkkatrom.dkweather.WeatherInfo;
+
+public class WeatherFragmentNew extends Fragment {
+
+    private TextView mCurrentTemp;
+    private TextView mTime;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+
+        View root = inflater.inflate(R.layout.weather_fragment, container, false);
+
+        mCurrentTemp = (TextView) root.findViewById(R.id.weather_fragment_current_temp);
+        mTime = (TextView) root.findViewById(R.id.weather_fragment_time);
+        return root;
+    }
+
+    public void updateContent(WeatherInfo weather) {
+        if (getActivity() == null || weather == null) {
+            return;
+        }
+        mCurrentTemp.setText(weather.getFormattedTemperature());
+        mTime.setText(weather.getTime());
+    }
+}
