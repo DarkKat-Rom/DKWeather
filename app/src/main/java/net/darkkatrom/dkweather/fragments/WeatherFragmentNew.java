1bc269cec1d16a2ff49db40577cf4b76ab73cfc3
XXMrHyde <xxmrhyde@gmx.co.uk>
2018-11-26 17:24:36 +0100

    Use RecyclerView for weather cards, (wip, part 2):
    Add bottom navigation bar:
    For now only show the settings button,
    other buttons doesn't make any sense atm

--- app/src/main/java/net/darkkatrom/dkweather/fragments/WeatherFragmentNew.java
+++ app/src/main/java/net/darkkatrom/dkweather/fragments/WeatherFragmentNew.java
@@ -17,29 +17,38 @@
 package net.darkkatrom.dkweather.fragments;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import net.darkkatrom.dkweather.R;
 import net.darkkatrom.dkweather.WeatherInfo;
+import net.darkkatrom.dkweather.activities.SettingsActivity;
 
-public class WeatherFragmentNew extends Fragment {
+public class WeatherFragmentNew extends Fragment implements OnClickListener {
 
+    View mRootView;
     private TextView mCurrentTemp;
     private TextView mTime;
+
+    private View mNavigationButtonSettings;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
 
-        View root = inflater.inflate(R.layout.weather_fragment, container, false);
+        mRootView = inflater.inflate(R.layout.weather_fragment, container, false);
 
-        mCurrentTemp = (TextView) root.findViewById(R.id.weather_fragment_current_temp);
-        mTime = (TextView) root.findViewById(R.id.weather_fragment_time);
-        return root;
+        mCurrentTemp = (TextView) mRootView.findViewById(R.id.weather_fragment_current_temp);
+        mTime = (TextView) mRootView.findViewById(R.id.weather_fragment_time);
+        setupBottomNavigation();
+
+        return mRootView;
     }
 
     public void updateContent(WeatherInfo weather) {
@@ -48,5 +57,31 @@ public class WeatherFragmentNew extends Fragment {
         }
         mCurrentTemp.setText(weather.getFormattedTemperature());
         mTime.setText(weather.getTime());
+    }
+
+    private void setupBottomNavigation() {
+        mRootView.findViewById(R.id.bottom_navigation_item_previous_day).setVisibility(View.INVISIBLE);
+        mNavigationButtonSettings = mRootView.findViewById(R.id.bottom_navigation_item_settings);
+        mNavigationButtonSettings.setOnClickListener(this);
+        mRootView.findViewById(R.id.bottom_navigation_item_next_day).setVisibility(View.INVISIBLE);
+
+        ImageView iv =
+                (ImageView) mNavigationButtonSettings.findViewById(R.id.bottom_navigation_item_icon);
+        TextView tv =
+                (TextView) mNavigationButtonSettings.findViewById(R.id.bottom_navigation_item_text);
+        iv.setImageResource(R.drawable.ic_action_settings);
+        iv.setImageTintList(getActivity().getColorStateList(
+                R.color.bottom_navigation_selectable_item_text_icon_color));
+        tv.setText(R.string.settings_title);
+        tv.setTextColor(getActivity().getColorStateList(
+                R.color.bottom_navigation_selectable_item_text_icon_color));
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mNavigationButtonSettings) {
+            Intent intent = new Intent(getActivity(), SettingsActivity.class);
+            startActivity(intent);
+        }
     }
 }
